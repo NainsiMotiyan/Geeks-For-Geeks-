5fe9ce902ef4b090c9ced1c7a38b8ade5b995fde
Nainsi Motiyan <nainsi759@gmail.com>
2025-12-15 17:58:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate by 90 degree/rotate-by-90-degree.cpp
@@ -0,0 +1,21 @@
+// User function template for C++
+
+/* matrix : given input matrix, you are require
+ to change it in place without using extra space */
+void rotate(vector<vector<int> >& mat) {
+    // Your code goes here
+    int n=mat.size();
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            swap(mat[i][j],mat[j][i]);
+        }
+    }
+    for(int i=0;i<n;i++){
+        int left=0,right=n-1;
+        while(left<right){
+            swap(mat[i][left],mat[i][right]);
+            left++;
+            right--;
+        }
+    }
+}
